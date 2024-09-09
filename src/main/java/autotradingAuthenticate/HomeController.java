0ed2b16a1108573788fbe6b03ec340f0e05a7f81
GoToBILL <wnqudwnrlrl@naver.com>
2024-09-09 14:53:08 +0900

    feat: add home html

---
+++ src/main/java/autotradingAuthenticate/HomeController.java
@@ -0,0 +1,13 @@
+package autotradingAuthenticate;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index"; // Thymeleaf 템플릿 파일명 (index.html)
+    }
+}
