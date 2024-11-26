e076e056522d2c236cb2ce436a83035d5b9c8997
GoToBILL <wnqudwnrlrl@naver.com>
2024-11-26 14:41:22 +0900

    feat: successMessage & errorMessage add 실패했을 때의 메시지를 enum으로 만들어 줬습니다.

--- src/main/java/autotradingAuthenticate/autotrading/exception/response/ErrorMessage.java
+++ src/main/java/autotradingAuthenticate/autotrading/exception/response/ErrorMessage.java
@@ -1,28 +1,48 @@
-package com.likelion.runtale.common.response;
+package autotradingAuthenticate.autotrading.exception.response;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.*;
 
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum ErrorMessage {
+    // 사용자 관련 실패 메시지
     USER_LOGIN_ID_NOT_EXIST(NOT_FOUND, "존재하지 않는 아이디"),
     USER_PASSWORD_NOT_EXIST(NOT_FOUND, "존재하지 않는 비밀번호"),
     USER_NOT_EXIST(NOT_FOUND, "존재하지 않는 회원"),
-    USER_LOGIN_ID_VALIDATE(BAD_REQUEST, "아이디 중복"),
-    USER_NOT_LOGGED_IN(HttpStatus.UNAUTHORIZED, "로그인이 필요합니다."),
-    USER_NOT_AUTHORIZED(HttpStatus.FORBIDDEN, "권한이 없습니다."),
-    USER_LOGIN_FAILED(HttpStatus.UNAUTHORIZED, "로그인 실패"),
-    RUNNING_NOT_FOUND(NOT_FOUND, "존재하지 않는 러닝 기록"),
-    TIER_NOT_FOUND(NOT_FOUND, "존재하지 않는 티어"),
-    SCENARIO_NOT_FOUND(NOT_FOUND, "존재하지 않는 시나리오"),
-    SCENARIO_STEP_NOT_FOUND(NOT_FOUND, "존재하지 않는 시나리오 스텝"),
-    UNEXPECTED_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "예상치 못한 오류가 발생했습니다.");
+    USER_LOGIN_ID_DUPLICATE(BAD_REQUEST, "아이디가 이미 존재합니다."),
+    USER_NOT_LOGGED_IN(UNAUTHORIZED, "로그인이 필요합니다."),
+    USER_NOT_AUTHORIZED(FORBIDDEN, "권한이 없습니다."),
+    USER_LOGIN_FAILED(UNAUTHORIZED, "로그인 실패"),
+    USER_SIGNUP_FAILED(BAD_REQUEST, "회원 가입 실패"),
+    USER_INFO_UPDATE_FAILED(BAD_REQUEST, "회원 정보 수정 실패"),
+
+    // 게시글 관련 실패 메시지
+    POST_NOT_FOUND(NOT_FOUND, "존재하지 않는 게시글"),
+    POST_CREATE_FAILED(BAD_REQUEST, "게시글 작성 실패"),
+    POST_UPDATE_FAILED(BAD_REQUEST, "게시글 수정 실패"),
+    POST_DELETE_FAILED(BAD_REQUEST, "게시글 삭제 실패"),
+    POST_LIST_FAILED(INTERNAL_SERVER_ERROR, "게시글 목록 조회 실패"),
+
+    // 댓글 관련 실패 메시지
+    COMMENT_NOT_FOUND(NOT_FOUND, "존재하지 않는 댓글"),
+    COMMENT_ADD_FAILED(BAD_REQUEST, "댓글 추가 실패"),
+    COMMENT_DELETE_FAILED(BAD_REQUEST, "댓글 삭제 실패"),
+
+    // 좋아요, 북마크 관련 실패 메시지
+    LIKE_ADD_FAILED(BAD_REQUEST, "좋아요 추가 실패"),
+    LIKE_REMOVE_FAILED(BAD_REQUEST, "좋아요 취소 실패"),
+    BOOKMARK_ADD_FAILED(BAD_REQUEST, "북마크 추가 실패"),
+    BOOKMARK_REMOVE_FAILED(BAD_REQUEST, "북마크 취소 실패"),
+
+    // 기타 실패 메시지
+    CATEGORY_NOT_FOUND(NOT_FOUND, "존재하지 않는 카테고리"),
+    CATEGORY_LIST_FAILED(INTERNAL_SERVER_ERROR, "카테고리 목록 조회 실패"),
+    UNEXPECTED_ERROR(INTERNAL_SERVER_ERROR, "예상치 못한 오류가 발생했습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
--- src/main/java/autotradingAuthenticate/autotrading/exception/response/SuccessMessage.java
+++ src/main/java/autotradingAuthenticate/autotrading/exception/response/SuccessMessage.java
@@ -1,4 +1,4 @@
-package com.likelion.runtale.common.response;
+package autotradingAuthenticate.autotrading.exception.response;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,17 +11,36 @@ import static org.springframework.http.HttpStatus.OK;
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum SuccessMessage {
+    // 사용자 관련 메시지
     USER_LOGIN_SUCCESS(OK, "로그인 성공"),
     USER_LOGOUT_SUCCESS(OK, "로그아웃 성공"),
     USER_SIGNUP_SUCCESS(CREATED, "회원 가입 성공"),
-    USER_INFO_CHANGE_SUCCESS(OK, "회원 정보 수정 성공"),
-    USER_INFO_FIND_SUCCESS(OK, "회원 조회 성공"),
+    USER_INFO_UPDATE_SUCCESS(OK, "회원 정보 수정 성공"),
+    USER_INFO_RETRIEVE_SUCCESS(OK, "회원 정보 조회 성공"),
     LOGIN_ID_VALIDATE_SUCCESS(OK, "아이디 중복 체크 성공"),
-    RUNNING_INFO_SUCCESS(OK,"러닝 정보 요청 성공"),
-    RUNNING_DELETE_SUCCESS(OK,"러닝 삭제 성공"),
-    TIER_INFO_SUCCESS(OK,"티어 정보 요청 성공"),
-    TIER_ASSIGN_SUCCESS(OK,"티어 할당 성공"),
-    SCENARIO_STEP_SUCCESS(OK,"시나리오 스텝 요청 성공"),;
+    PASSWORD_RESET_SUCCESS(OK, "비밀번호 재설정 성공"),
+    PASSWORD_CHANGE_SUCCESS(OK, "비밀번호 변경 성공"),
+
+    // 게시글 관련 메시지
+    POST_CREATE_SUCCESS(CREATED, "게시글 작성 성공"),
+    POST_READ_SUCCESS(OK, "게시글 조회 성공"),
+    POST_UPDATE_SUCCESS(OK, "게시글 수정 성공"),
+    POST_DELETE_SUCCESS(OK, "게시글 삭제 성공"),
+    POST_LIST_SUCCESS(OK, "게시글 목록 조회 성공"),
+
+    // 댓글 관련 메시지
+    COMMENT_ADD_SUCCESS(CREATED, "댓글 추가 성공"),
+    COMMENT_DELETE_SUCCESS(OK, "댓글 삭제 성공"),
+    COMMENT_LIST_SUCCESS(OK, "댓글 목록 조회 성공"),
+
+    // 좋아요, 북마크 관련 메시지
+    LIKE_ADD_SUCCESS(OK, "게시글 좋아요 추가 성공"),
+    LIKE_REMOVE_SUCCESS(OK, "게시글 좋아요 취소 성공"),
+    BOOKMARK_ADD_SUCCESS(OK, "게시글 북마크 추가 성공"),
+    BOOKMARK_REMOVE_SUCCESS(OK, "게시글 북마크 취소 성공"),
+
+    // 기타
+    CATEGORY_LIST_SUCCESS(OK, "카테고리 목록 조회 성공"),;
 
     private final HttpStatus httpStatus;
     private final String message;
